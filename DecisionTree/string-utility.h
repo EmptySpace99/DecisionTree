b4e78a48dc4d9013f1f92b5f7216b4b5caada35c
Filippo Chirigoni <62540354+EmptySpace99@users.noreply.github.com>
2021-06-14 02:46:48 +0200

    Delete DecisionTree directory

--- DecisionTree/string-utility.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <sstream>
-
-using namespace std;
-
-void removeBrackets(string&);        //remove brackets from string
-void removeCommas(string&);          //remove commas from string
-void removeBlanksandOther(string&);  //remove spaces, brackets and commas from string
-int str2int(const string&);          // convert string in number
